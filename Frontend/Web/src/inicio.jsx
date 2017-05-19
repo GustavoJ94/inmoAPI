165446e2924608fc1e2775d2de6143ceab3c29f9
Aldrin Salazar <salazar.aldrin@gmail.com>
2017-05-19 18:51:00 -0400

    material

--- Frontend/Web/src/inicio.jsx
+++ Frontend/Web/src/inicio.jsx
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class Inicio extends Component {
     render(){
+    	const papelEstilo = {
+			  height: 100,
+			  width: 100,
+			  margin: "auto",
+			  textAlign: 'center',
+			};
+
         return (
         	<MuiThemeProvider>
 	        	<div>
 	        		<AppBar title="Inmo" />
-	        		<RaisedButton label="Hola" primary={true}/>
+	        		<Paper style={papelEstilo} zDepth={2} circle={true}>
+	        			<h3>Hola mundo</h3>
+	        		</Paper>
+	        		<RaisedButton label="Un boton" primary={true} />
 	        	</div>
         	</MuiThemeProvider>
         	);
