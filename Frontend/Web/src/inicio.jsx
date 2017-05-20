beb87b8f8c99581bc2686c0382c19f423067b8bd
Aldrin Salazar <salazar.aldrin@gmail.com>
2017-05-19 23:22:02 -0400

    ejemplo

--- Frontend/Web/src/inicio.jsx
+++ Frontend/Web/src/inicio.jsx
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, IndexLink, hashHistory, Link } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import Paper from 'material-ui/Paper';
-import RaisedButton from 'material-ui/RaisedButton';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+///
+import Divider from 'material-ui/Divider';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+///
+import Barra from './componentes/barra.jsx';
 
+injectTapEventPlugin();
 class Inicio extends Component {
     render(){
-    	const papelEstilo = {
-			  height: 100,
-			  width: 100,
-			  margin: "auto",
-			  textAlign: 'center',
-			};
+    	const items = [
+  							<MenuItem key={1} value={1} primaryText="Terreno" />,
+				        	<MenuItem key={2} value={2} primaryText="Local" />,
+				        	<MenuItem key={3} value={3} primaryText="Apartamento" />,
+				          	<MenuItem key={4} value={4} primaryText="Casa" />];
 
         return (
         	<MuiThemeProvider>
-	        	<div>
-	        		<AppBar title="Inmo" />
-	        		<Paper style={papelEstilo} zDepth={2} circle={true}>
-	        			<h3>Hola mundo</h3>
-	        		</Paper>
-	        		<RaisedButton label="Un boton" primary={true} />
-	        	</div>
+        		<div>
+        			<Barra/>
+        			<img src="img/logo-g.png" height="200" width="200" style={{"margin":"auto", "display":"block"}}/>
+        			<Divider/>
+        			<div>
+        				<h2> Estoy buscando </h2>
+        				<SelectField floatingLabelText="Tipo de inmueble" value={2}>
+        					{items}
+				        </SelectField>
+        			</div>
+        		</div>
         	</MuiThemeProvider>
         	);
     }
